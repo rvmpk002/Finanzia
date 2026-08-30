930fc3add5ce3e90bccbe41e808df6a594cc720a
Rafael <ing.rafaelvm@gmail.com>
2026-08-30 17:20:43 -0600

    nu tooltip

--- src/warningRules.ts
+++ src/warningRules.ts
@@ -9,5 +9,5 @@ export const shouldShowMercadoPagoMinimumBalanceWarning = (date: Date) => {
   return day >= lastDayOfMonth - 2 && day <= lastDayOfMonth;
 };
 
-export const nuMinimumPurchaseWarningLabel = "Has una compra minima para mantener el 13%";
+export const nuMinimumPurchaseWarningLabel = "Has una compra para mantener la tasa del 13%";
 export const mercadoPagoMinimumBalanceWarningLabel = "Ingresa 3000 para mantener el 12%";
